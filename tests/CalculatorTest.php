af3976a3a44a2df84f604a77084e4b08b3838b80
Your Name <you@example.com>
2024-04-18 13:12:19 +0200

    init

---
+++ tests/CalculatorTest.php
@@ -0,0 +1,9 @@
+<?php
+class CalculatorTest extends TestCase
+{
+    public function testAdd()
+    {
+        $calculator = new Calculator();
+        $this->assertEquals(4, $calculator->add(2, 2));
+    }
+}
